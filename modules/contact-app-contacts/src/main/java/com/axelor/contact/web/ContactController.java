c73c0d5b3a35136c7fc3b1ed72d22ed1c7379a46
kpa-axelor <kpa.axelor@gmail.com>
2019-09-04 16:25:33 +0530

    contact app complete

--- modules/contact-app-contacts/src/main/java/com/axelor/contact/web/ContactController.java
+++ modules/contact-app-contacts/src/main/java/com/axelor/contact/web/ContactController.java
@@ -17,14 +17,12 @@ import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
 
 public class ContactController {
-			
-	
-	public void printdata(ActionRequest request, ActionResponse response)
-	{
+
+	public void printdata(ActionRequest request, ActionResponse response) {
 		Price price = request.getContext().asType(Price.class);
 		System.out.println(price.getPrice());
 		System.out.println(price.getQuantity());
-		
+
 //		System.out.println(request.getContext().entrySet());
 //		Map map = new HashMap();
 //		Set set = (Set) request.getContext().entrySet();
@@ -34,45 +32,60 @@ public class ContactController {
 //			 Map.Entry entry = (Map.Entry)itr.next();
 //			 System.out.println(entry.getKey()+" "+entry.getValue());
 //		 }
-		
-		 
-		response.setValue("totalAmount", (price.getPrice()*price.getQuantity()));
+
+		response.setValue("totalAmount", (price.getPrice() * price.getQuantity()));
 	}
-	
-	public void reportvalue(ActionRequest request, ActionResponse response)
-	{
-		System.out.println(request.getContext().get("_ids"));
-		List<Integer> list = (List<Integer>) request.getContext().get("_ids");
-		System.out.println(list);
-		String s1 ="";
-		for(int i=0;i<list.size();i++)
-		{
-				s1 = s1 + list.get(i)+",";			
+
+	public void reportvalue(ActionRequest request, ActionResponse response) {
+
+		System.out.println(request.getContext().entrySet());
+		System.out.println("final=>" + request.getContext().get("_ids"));
+		List<Integer> ids = (List<Integer>) request.getContext().get("_ids");
+		System.out.println(ids);
+
+		
+		if (ids == null) {
+			System.out.println("form value" + request.getContext().entrySet());
+			System.out.println(request.getContext().get("id"));
+			Long i = (Long) request.getContext().get("id");
+			String s3 = i.toString(i);
+			request.getContext().put("contactId", s3);
+			System.out.println(request.getContext().get("contactId"));
 		}
-		String s = (String) s1.subSequence(0, s1.length()-1);
-		System.out.println("string="+s);
-		
-		 String filePath = AppSettings.get().get("file.upload.dir");
-		 System.out.println("filePath="+filePath);
-		 System.out.println("file put="+request.getContext().put("photoPath",filePath));
-		 System.out.println(request.getContext().get("photoPath"));
-//		Object[] ol =  list.toArray();
-//		System.out.println("ArrayObj length ="+ol.length);
-		System.err.println("req class == >"+request.getContext().put("contactId", s));
-		System.out.println(request.getContext().get("contactId"));
+		else if (!ids.isEmpty()) {
+
+			System.out.println(ids);
+			String s1 = "";
+			for (int i = 0; i < ids.size(); i++) {
+				s1 = s1 + ids.get(i) + ",";
+			}
+			String s = (String) s1.subSequence(0, s1.length() - 1);
+			System.out.println("string=" + s);
+			System.err.println("req class == >" + request.getContext().put("contactId", s));
+			System.out.println(request.getContext().get("contactId"));
+
+		}
+
+		String filePath = AppSettings.get().get("file.upload.dir");
+		System.out.println("filePath=" + filePath);
+		System.out.println("file put=" + request.getContext().put("photoPath", filePath));
+		System.out.println(request.getContext().get("photoPath"));
+//		Object[] ol = ids.toArray();
+//		System.out.println("ArrayObj length =" + ol.length);
+
 	}
-	public void formValue(ActionRequest request , ActionResponse response)
-	{
-		System.out.println("form value"+request.getContext().entrySet());
-		System.out.println(request.getContext().get("id"));
-		Long i = (Long) request.getContext().get("id");
-		String s=i.toString(i);
-		request.getContext().put("contactId",s);		
-		 String filePath = AppSettings.get().get("file.upload.dir");
-		 System.out.println("filePath="+filePath);
-		 System.out.println("file put="+request.getContext().put("photoPath",filePath));
-		 System.out.println(request.getContext().get("photoPath"));
-			
-	}
-	
+
+//	public void formValue(ActionRequest request, ActionResponse response) {
+//		System.out.println("form value" + request.getContext().entrySet());
+//		System.out.println(request.getContext().get("id"));
+//		Long i = (Long) request.getContext().get("id");
+//		String s = i.toString(i);
+//		request.getContext().put("contactId", s);
+//		String filePath = AppSettings.get().get("file.upload.dir");
+//		System.out.println("filePath=" + filePath);
+//		System.out.println("file put=" + request.getContext().put("photoPath", filePath));
+//		System.out.println(request.getContext().get("photoPath"));
+//
+//	}
+
 }
---
+++ modules/contact-app-contacts/src/main/java/com/axelor/contact/web/ITranslation.java
@@ -0,0 +1,5 @@
+package com.axelor.contact.web;
+
+public class ITranslation {
+	  public static final String CONTACTREPORT_COMPANY_NAME = /*$$(*/ "value:companyName"; /*)*/
+}
